18f3fed245c1d404d3eb9a143447194ffcadea23
FelipeSSDev <felipedevbrasil@gmail.com>
2021-11-09 23:45:42 -0300

    feat: Create home page style

---
+++ styles/home.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+import { mainColors } from "../constants/colors";
+
+export const MainContent = styled.main`
+  background-color: ${mainColors.primary};
+  color: white;
+  padding: 1.5rem;
+  margin: 3em;
+  min-height: 500px;
+`;
